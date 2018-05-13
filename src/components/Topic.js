19fb5dff175233654f8251ed13425d62f6ea8bcd
Claudia Alphonse <claudialphonse@gmail.com>
2018-05-13 17:32:28 +0530

    routing

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import {
+  Link,
+  Route
+} from 'react-router-dom';
 
 export default function Topic(){
-  return <div>TOPICS</div>
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to='/topics/rendering'>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to='/topics/components'>Components</Link>
+        </li>
+        <li>
+          <Link to='/topics/props-v-state'>Props Vs State</Link>
+        </li>
+      </ul>
+      <hr/>
+      <Route path='/topics'></Route>
+    </div>
+  ) 
 }
